bc463240ef73bab0d4a8cd1a0b45411da669dc6f
Payam Naderi <naderi.payam@gmail.com>
2014-02-10 18:22:18 +0330

    describe usage with some comment

--- config/module.config.php
+++ config/module.config.php
@@ -1,20 +1,30 @@
 <?php
 return array(
-	'yima_widgetator' => array(
-        /*
-         * This is configurable service manager config
-         */
+    /**
+     * Register Widgets in WidgetManager
+     *
+     * each widget must instance of WidgetInterface
+     */
+    'yima_widgetator' => array(
+         // This is configurable service manager config
 		'invokables' => array(
-			// 'widgetName' => 'Widget\Class',
+			# 'widgetName' => 'Widget\Class',
 		),
 	),
 
+    /**
+     * Libraries that used in Ajax Loading of widgets.
+     * @see \yimaJquery\View\Helper\WidgetAjaxy
+     */
     'static_uri_helper' => array(
         'Yima.Widgetator.JS.Jquery.Ajaxq' => '{{basepath}}/yima-widgetator/js/jquery.ajaxq.min.js',
         'Yima.Widgetator.JS.Jquery.Json'  => '{{basepath}}/yima-widgetator/js/jquery.json.min.js',
     ),
 
-	'controllers' => array(
+    /**
+     * Rest Controller to loading widgets
+     */
+    'controllers' => array(
 		'invokables' => array(
             'yimaWidgetator\Controller\Rest' => 'yimaWidgetator\Controller\WidgetLoadRestController'
 		),
