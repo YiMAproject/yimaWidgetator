f882b7ce6ae7a6b80ebd5fe8165ec86935952e47
Payam Naderi <naderi.payam@gmail.com>
2014-08-29 11:08:01 +0430

    improve codes

--- src/yimaWidgetator/View/Helper/WidgetAjaxy.js
+++ src/yimaWidgetator/View/Helper/WidgetAjaxy.js
@@ -19,30 +19,7 @@
 
         if ((!options.method || options.method == 'render') && !options.callback) {
             // use default callback on render if not present
-            defaults.callback = function(element, response) {
-                element.html(response.result.content);
-
-                for (var i in response.result.links) {
-                    if ($('head').find('link[href=\"' + response.result.links[i] + '\"]').length == 0) {
-                        $('<link rel=\"'+response.result.links[i].rel+'\" type=\"'+response.result.links[i].type+'\" href=\"' + response.result.links[i].href + '\" />').appendTo('head');
-                    }
-                }
-
-                for (var i in response.result.scripts) {
-                    if (response.result.scripts[i] != null) {
-                        if (response.result.scripts[i].source) {
-                            $('<script type="'+response.result.scripts[i].type+'">'+response.result.scripts[i].source+'</script>').appendTo('body');
-                        } else if (response.result.scripts[i].attributes.src) {
-                            // this is a file
-                            if ($('body').find('script[src=\"'+response.result.scripts[i].attributes.src+'\"]').length == 0) {
-                                if ($('head').find('script[src=\"'+response.result.scripts[i].attributes.src+'\"]').length == 0) {
-                                    $('<script type="'+response.result.scripts[i].type+'" src="'+response.result.scripts[i].attributes.src+'"></script>').appendTo('body');
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            defaults.callback = $.fn.widgetator.defaultCallback;
         }
 
         var exOptions = $.extend(false, defaults, options);
@@ -76,6 +53,31 @@
     $.fn.widgetator.settings = {
         provider_url: '{{url}}',
         loading_class: 'widgetator-loading'
+    };
+
+    $.fn.widgetator.defaultCallback = function (element, response) {
+        element.html(response.result.content);
+
+        for (var i in response.result.links) {
+            if ($('head').find('link[href=\"' + response.result.links[i] + '\"]').length == 0) {
+                $('<link rel=\"'+response.result.links[i].rel+'\" type=\"'+response.result.links[i].type+'\" href=\"' + response.result.links[i].href + '\" />').appendTo('head');
+            }
+        }
+
+        for (var i in response.result.scripts) {
+            if (response.result.scripts[i] != null) {
+                if (response.result.scripts[i].source) {
+                    $('<script type="'+response.result.scripts[i].type+'">'+response.result.scripts[i].source+'</script>').appendTo('body');
+                } else if (response.result.scripts[i].attributes.src) {
+                    // this is a file
+                    if ($('body').find('script[src=\"'+response.result.scripts[i].attributes.src+'\"]').length == 0) {
+                        if ($('head').find('script[src=\"'+response.result.scripts[i].attributes.src+'\"]').length == 0) {
+                            $('<script type="'+response.result.scripts[i].type+'" src="'+response.result.scripts[i].attributes.src+'"></script>').appendTo('body');
+                        }
+                    }
+                }
+            }
+        }
     }
 
 })(jQuery);
